f8bd4c5d9c3a08559d065d737aad9c6836219606
Alex-Desktop <alex.40440@gmail.com>
2024-05-07 21:10:53 -0700

    usable portals for level loading and working lives system, requires scene manager

--- Assets/Scenes/Player/Scripts/PlayerHit.cs
+++ Assets/Scenes/Player/Scripts/PlayerHit.cs
@@ -1,23 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-public class HealthSplash : MonoBehaviour
+
+public class PlayerHit : MonoBehaviour
 {
-    public TextMeshProUGUI display;
     private SceneLoader loader;
     // Start is called before the first frame update
     void Start()
     {
-        display = GetComponent<TextMeshProUGUI>();
         loader = GameObject.Find("SceneManager").GetComponent<SceneLoader>();
-        display.text = "X " + loader.lives;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
+        if (other.tag == "enemyAttack")
+            loader.died();
     }
 
 }
